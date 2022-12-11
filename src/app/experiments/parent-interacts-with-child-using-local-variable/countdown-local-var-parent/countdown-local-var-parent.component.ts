243628114a34f7bf1e29a46d5a06d007f6fa5b41
vitoneto <vito.neto@sistemasigma.com>
2022-12-11 17:56:29 -0300

    Make timer works.

--- src/app/experiments/parent-interacts-with-child-using-local-variable/countdown-local-var-parent/countdown-local-var-parent.component.ts
+++ src/app/experiments/parent-interacts-with-child-using-local-variable/countdown-local-var-parent/countdown-local-var-parent.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { CountdownTimerComponent } from '../countdown-timer/countdown-timer.component';
 
 @Component({
-  selector: 'app-countdown-local-var-parent',
-  templateUrl: './countdown-local-var-parent.component.html',
-  styleUrls: ['./countdown-local-var-parent.component.scss']
+	selector: 'app-countdown-local-var-parent',
+	templateUrl: './countdown-local-var-parent.component.html',
+	styleUrls: ['./countdown-local-var-parent.component.scss']
 })
 export class CountdownLocalVarParentComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
