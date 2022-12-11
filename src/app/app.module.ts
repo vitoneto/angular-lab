c0e8fdd949f89ec514f5a236c65e8f7d56ebc5c2
vitoneto <vito.neto@sistemasigma.com>
2022-12-11 16:34:06 -0300

    Fixed the structure of the experiments folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LabOverviewComponent } from './pages/lab-overview/lab-overview.component';
-import { ParentInteractsWithChildUsingLocalVariableComponent } from './experiments/parent-interacts-with-child-using-local-variable/parent-interacts-with-child-using-local-variable.component';
+import { CountdownLocalVarParentComponent } from './experiments/parent-interacts-with-child-using-local-variable/countdown-local-var-parent/countdown-local-var-parent.component';
+import { CountdownTimerComponent } from './experiments/parent-interacts-with-child-using-local-variable/countdown-timer/countdown-timer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LabOverviewComponent,
-    ParentInteractsWithChildUsingLocalVariableComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LabOverviewComponent,
+		CountdownLocalVarParentComponent,
+  CountdownTimerComponent
+	],
+	imports: [BrowserModule, AppRoutingModule],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/experiments/parent-interacts-with-child-using-local-variable/countdown-local-var-parent/countdown-local-var-parent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-countdown-local-var-parent',
+  templateUrl: './countdown-local-var-parent.component.html',
+  styleUrls: ['./countdown-local-var-parent.component.scss']
+})
+export class CountdownLocalVarParentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
